369814385e549e37a79d29d3cb6c1d1e163c7b27
greenlikeorange <beginofalove@hotmail.com>
2014-10-25 16:20:42 +0630

    Last Update

---
+++ config2.js
@@ -0,0 +1,29 @@
+var TOTAL_WINNERS = 5;
+var PLAY_TIME = 7000;
+var LOOP = 5;
+
+var GIFT_FILTER = true;
+
+/*
+ * Abbr is need for Filtering winner list
+ * abbr don't allow space and small letter is better
+ */
+var gifts = [
+  // { name: "Github", abbr:"github", icon: "fa fa-github" },
+  { name: "Genius Coffee", abbr:"coffee", icon: "fa fa-coffee" }
+];
+
+var attendants = [
+  "Ye Lin Aung",
+  "Ye Mon Kyaw",
+  "Swan Htet Aung",
+  "Zaw Myo Htet",
+  "Naing Ye Min",
+  "Naing Lin Aung",
+  "Nyan Lynn Htut",
+  "Yan Naing",
+  "Ye Myat Thu",
+  "Thuya Myo Nyunt",
+  "Arkar Aung",
+  "Arkar Min Aung",
+];
